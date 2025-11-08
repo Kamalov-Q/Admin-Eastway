a7c4d6d9b8f9c94a7a3337444d30f846830c60f2
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-11-09 01:46:10 +0500

    subdomain added

--- src/pages/HotelCategories.tsx
+++ src/pages/HotelCategories.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
-import toast from "react-hot-toast";
+import toast from "react-hot-toast"; 
 
 import {
   useHotelCategories,
