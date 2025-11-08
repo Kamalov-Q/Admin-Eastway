911613db7542ea5dda7696fd204073bb1edd3c5c
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-11-09 02:09:25 +0500

    subdomain added

--- src/pages/HotelCategories.tsx
+++ src/pages/HotelCategories.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import toast from "react-hot-toast"; 
-
+ 
 import {
   useHotelCategories,
   useCreateHotelCategory,
